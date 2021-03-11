d425a64ae569cec7883b37a259071c1caf5ab759
Evan Sauer <ESAUER5@hallmark.com>
2021-03-11 12:31:43 -0600

    modified hello world

--- crm.lua
+++ crm.lua
@@ -1,7 +1,33 @@
-crm = { }
+-- (1)
+local f = CreateFrame("Frame", "CRMFrame", UIParent)
+f:SetSize(400, 400)
+f:SetPoint("CENTER")
 
-function crm:HelloWorld()
-    message("Hello World!")
-end
+-- (2)
+f:SetBackdrop({
+	bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+	edgeFile = "Interface\\ChatFrame\\ChatFrameBackground",
+	edgeSize = 1,
+})
+f:SetBackdropColor(0, 0, 0, .5)
+f:SetBackdropBorderColor(0, 0, 0)
 
-Sandbox:HelloWorld()
+-- (3)
+f:EnableMouse(true)
+f:SetMovable(true)
+f:RegisterForDrag("LeftButton")
+f:SetScript("OnDragStart", f.StartMoving)
+f:SetScript("OnDragStop", f.StopMovingOrSizing)
+f:SetScript("OnHide", f.StopMovingOrSizing)
+
+-- (4)
+local close = CreateFrame("Button", "CRMCloseButton", f, "UIPanelCloseButton")
+close:SetPoint("TOPRIGHT", f, "TOPRIGHT")
+close:SetScript("OnClick", function()
+	f:Hide()
+end)
+
+-- (5)
+local text = f:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+text:SetPoint("CENTER")
+text:SetText("Hello World!")
